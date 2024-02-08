56be01646316b28a50b7015f9e6fc3456ab666b2
wty-yy <wty-yy@github.com>
2024-02-08 13:49:47 +0800

    add dataset_verion.py

---
+++ katacr/build_dataset/dataset_version.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+from katacr.build_dataset.utils.datapath_manager import PathManager
+import pandas as pd
+import numpy as np
+from katacr.constants.label_list import idx2unit
+from katacr.utils import colorstr
+
+class DatasetManager:
+  def __init__(self):
+    self.path_manager = PathManager()
+    paths = sorted(list(Path(__file__).parent.glob('*.txt')))
+    self.path_version = paths[-1] if len(paths) else None
+    self.version = self.path_version.stem if self.path_version else None
+  
+  def update(self):
+    paths = self.path_manager.search("images", "segment")
+    self.segment = {}
+    for p in paths:
+      try:
+        if p.stem.count('_') <= 1:  # Don't have side
+          side = 0
+        else: side = p.stem.split('_')[1]
+        name = p.parent.name
+        if name not in self.segment:
+          self.segment[name] = [0, 0]
+        self.segment[name][int(side)] += 1
+      except Exception as e:
+        print(f"ERROR Path: {p}")
+        raise e
+    
+    self.annotation = {}
+    paths = self.path_manager.search("images", 2, regex="^\d+.txt")
+    for p in paths:
+      with p.open('r') as file:
+        box = np.loadtxt(file)
+      for b in box:
+        name, side = idx2unit[int(b[0])], int(b[5])
+        if name not in self.annotation:
+          self.annotation[name] = [0, 0]
+        self.annotation[name][side] += 1
+  
+  def save(self, part=['annotation', 'segment']):
+    if not isinstance(part, (list, tuple)): part = [part]
+    root_path = self.path_manager.path / "version_info"
+    root_path.mkdir(exist_ok=True)
+    for pt in part:
+      data: dict = eval("self."+pt)
+      print(colorstr("Process part: ") + colorstr('red', pt) + "...")
+      paths = list(root_path.glob(pt + '*'))
+      if len(paths) == 0:
+        new_version = '0.1'
+        print("Don't find any old version, start from v0.1")
+      else:
+        old_version = paths[-1].stem.split('_')[-1][1:]
+        new_version = '0.' + str(int(old_version.split('.')[1])+1)
+        print(f"Found old version {colorstr('red', 'v'+old_version)}")
+        df = pd.read_csv(str(paths[-1]), index_col=0)
+        same_flag, diff_count, note_file = True, 0, None
+        for i in data:
+          new = np.array(data.get(i, []))
+          if i not in df.index:
+            old = np.zeros((2,), dtype=np.int32)
+          else:
+            old = np.array(df.loc[i])
+          if not (old == new).all():
+            if same_flag:
+              print("Find following difference between the two versions:")
+              note_file = (root_path / f"{pt}_v{new_version}_update.txt").open('w')
+              s = ' ' * 21 + f"{'Old (v'+old_version+')':>16} -> {'New (v'+new_version+')':>16} {'Count':>8}"
+              note_file.write(s + '\n')
+              print(s)
+            diff_count += 1
+            s = f"{i:<20} {str(old):>16} -> {str(new):>16} {diff_count:>8}"
+            note_file.write(s + '\n')
+            print(s)
+            same_flag = False
+        if note_file is not None: note_file.close()
+        if same_flag:
+          print(f"Version information is same, skip to save")
+          continue
+
+      df = pd.DataFrame(data).T
+      save_path = root_path / (pt + '_v' + new_version + '.csv')
+      df.to_csv(str(save_path))
+      print("Save to " + colorstr('green', save_path))
+
+if __name__ == '__main__':
+  manager = DatasetManager()
+  manager.update()
+  manager.save()
--- katacr/utils/__init__.py
+++ katacr/utils/__init__.py
@@ -3,24 +3,49 @@ import numpy as np
 import contextlib, time
 
 def load_image_array(
-        path_image, to_gray=False,
-        keep_dim=True, resize=None
-    ):
-    image = Image.open(path_image)
-    if resize is not None: image = image.resize(resize)
-    if to_gray: image = image.convert("L")
-    image = np.array(image)
-    if keep_dim and image.ndim == 2: image = image[..., None]
-    return image 
+    path_image, to_gray=False,
+    keep_dim=True, resize=None
+  ):
+  image = Image.open(path_image)
+  if resize is not None: image = image.resize(resize)
+  if to_gray: image = image.convert("L")
+  image = np.array(image)
+  if keep_dim and image.ndim == 2: image = image[..., None]
+  return image 
 
 class Stopwatch(contextlib.ContextDecorator):
-    def __init__(self, t=0.0):
-        self.t = t
-    
-    def __enter__(self):
-        self.start = time.time()
-        return self
-    
-    def __exit__(self, type, value, traceback):
-        self.dt = time.time() - self.start
-        self.t += self.dt
+  def __init__(self, t=0.0):
+    self.t = t
+  
+  def __enter__(self):
+    self.start = time.time()
+    return self
+  
+  def __exit__(self, type, value, traceback):
+    self.dt = time.time() - self.start
+    self.t += self.dt
+
+def colorstr(*input):
+  # Colors a string https://en.wikipedia.org/wiki/ANSI_escape_code, i.e.  colorstr('blue', 'hello world')
+  *args, string = input if len(input) > 1 else ('blue', 'bold', input[0])  # color arguments, string
+  colors = {
+    'black': '\033[30m',  # basic colors
+    'red': '\033[31m',
+    'green': '\033[32m',
+    'yellow': '\033[33m',
+    'blue': '\033[34m',
+    'magenta': '\033[35m',
+    'cyan': '\033[36m',
+    'white': '\033[37m',
+    'bright_black': '\033[90m',  # bright colors
+    'bright_red': '\033[91m',
+    'bright_green': '\033[92m',
+    'bright_yellow': '\033[93m',
+    'bright_blue': '\033[94m',
+    'bright_magenta': '\033[95m',
+    'bright_cyan': '\033[96m',
+    'bright_white': '\033[97m',
+    'end': '\033[0m',  # misc
+    'bold': '\033[1m',
+    'underline': '\033[4m'}
+  return ''.join(colors[x] for x in args) + f'{string}' + colors['end']
