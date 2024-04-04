2a1b9c81a053a1bce580d78424378cbb82d4641d
wty-yy <wty-yy@github.com>
2024-04-04 13:35:14 +0800

    update

--- katacr/yolov8/cfg.py
+++ katacr/yolov8/cfg.py
@@ -6,7 +6,7 @@ train_datasize = 20000
 img_size = (576, 896)  # width, height
 assert img_size[0] % 32 == 0 and img_size[1] % 32 == 0, "The shape must be 32 multiple"
 
-base_idxs = 13
+base_idxs = 14
 max_detect_num = 85  # 160  # 85  # 65
 num_detector = 2  # 1  # 2  # 3
 detector1_list = list(range(base_idxs)) + list(range(base_idxs, max_detect_num))
