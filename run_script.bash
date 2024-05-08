32d56dc4ebb6b5c1b23bdf31069dfac3385f68c9
wty-yy <wty-yy@github.com>
2024-05-08 11:19:28 +0800

    update

--- run_script.bash
+++ run_script.bash
@@ -4,7 +4,7 @@
 # CUDA_VISIBLE_DEVICES=$1 python katacr/detection/train.py --train --wandb-track $2
 # CUDA_VISIBLE_DEVICES=$1 python katacr/policy/offline/train.py --wandb --total-epochs 10 --batch-size 32 --nominal-batch-size 128 \
 #   --name "StARformer_v0.2_golem_ai" --replay-dataset "/data/user/zhihengwu/Coding/dataset/clash-royale-replay-dataset/golem_ai" $2
-CUDA_VISIBLE_DEVICES=$1 python katacr/policy/offline/train.py --wandb --total-epochs 10 --batch-size 32 --nominal-batch-size 128 \
+CUDA_VISIBLE_DEVICES=$1 python katacr/policy/offline/train.py --wandb --total-epochs 10 --batch-size 16 --nominal-batch-size 128 \
   --name "StARformer_v0.3_golem_ai" $2
 # CUDA_VISIBLE_DEVICES=$1 python katacr/policy/offline/train.py --wandb --total-epochs 10 --batch-size 32 --nominal-batch-size 128 --name "StARformer_same_action_shuffle__random_interval" $2
 # CUDA_VISIBLE_DEVICES=$1 python katacr/policy/offline/train.py --wandb --learning-rate 0.0006 --total-epochs 30 --batch-size 32 --nominal-batch-size 128 --replay-dataset /data/user/zhihengwu/Coding/dataset/clash-royale-replay-dataset --name "StARformer_0.1" $2
